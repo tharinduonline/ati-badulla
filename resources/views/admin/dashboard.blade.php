68b52cfa88d87c1cee3a8da49bbbcbb75e853b10
Tharindu Chathuranga <tharinduchathuranga1216@gmail.com>
2019-02-28 10:01:31 +0530

    removed album counter

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -52,16 +52,7 @@
                 </div>
             </div>
 
-               <div class="col-lg-3">
-                <div class="panel panel-info">
-                    <div class="panel-heading text-center">
-                         ALBUMS
-                    </div>
-                    <div class="panel-body">
-                        <h1 class="text-center">{{ $galleries_count }}</h1>
-                    </div>
-                </div>
-            </div>
+               
     <div class="col-lg-3">
         <div class="panel panel-info">
             <div class="panel-heading text-center">
